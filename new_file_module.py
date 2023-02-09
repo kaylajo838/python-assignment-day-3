f3e38969171cb40b22d3d46c8dccde283108b32d
Kayla Imming <kaylaimming@gmail.com>
2023-02-09 09:49:00 -0800

    completed homework assignment

---
+++ new_file_module.py
@@ -0,0 +1,11 @@
+import math
+
+def square_footage(length, width):
+    # square footage area formula = length * width
+    area = length * width
+    return area 
+
+def circle_circumference(radius):
+    # circumference formula = 2*pi*radius
+    circumference = 2 * math.pi * radius
+    return circumference
